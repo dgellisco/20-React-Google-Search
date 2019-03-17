c36a00169dba233182f19fe5a8e7d8add9bf5ca0
David Ellis <dgellis@live.com>
2019-03-17 11:54:27 -0600

    working app

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a href="https://dgellisco.github.io/" class="nav-link">
+        David Ellis
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
