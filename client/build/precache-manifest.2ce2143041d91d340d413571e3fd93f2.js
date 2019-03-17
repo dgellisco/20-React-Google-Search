500be366c1a9a69f7765cdec148617713ae8bab5
David Ellis <dgellis@live.com>
2019-03-17 12:12:06 -0600

    add production build

---
+++ client/build/precache-manifest.2ce2143041d91d340d413571e3fd93f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f392bfac87159be7a3e0",
+    "url": "/static/js/main.e2df68cc.chunk.js"
+  },
+  {
+    "revision": "be5b03084a5faa5156fb",
+    "url": "/static/js/2.c0b55515.chunk.js"
+  },
+  {
+    "revision": "f392bfac87159be7a3e0",
+    "url": "/static/css/main.635807e7.chunk.css"
+  },
+  {
+    "revision": "1da67d10a40a81f70e67615d7d2ae484",
+    "url": "/index.html"
+  }
+];
