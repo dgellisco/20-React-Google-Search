04ef6a65faae15018918aab126b78ee991425e31
David Ellis <dgellis@live.com>
2019-03-17 12:41:49 -0600

    add static json

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,12 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-const express = require("express");
-const app = express();
-
 // API Routes
 router.use("/api", apiRoutes);
 
-// // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-const root = require('path').join(__dirname, 'client', 'build')
-app.use(express.static(root));
-app.get("*", (req, res) => {
-    res.sendFile('index.html', { root });
-})
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
