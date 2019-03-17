1f38e645ab5ac988ce3cc00528ab63809e6aa00c
David Ellis <dgellis@live.com>
2019-03-17 12:32:57 -0600

    server routes

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,21 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
+const express = require("express");
+const app = express();
+
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// // If no API routes are hit, send the React app
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+const root = require('path').join(__dirname, 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
 
 module.exports = router;
